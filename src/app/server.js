d371a27c6597fa72f76b220c45363c709ddf06b7
priya-UI24 <85362475+priya-UI24@users.noreply.github.com>
2021-06-07 11:01:58 +0530

    Update server.js

--- src/app/server.js
+++ src/app/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/testapp-ui'));
+app.use(express.static(__dirname + '/dist/testapp_ui'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/testapp-ui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/testapp_ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
